5063f7b02f516a60029bbf0b97c1024aaeb8b1fc
Alejandro Escontrela <alejandroescontrela@gmail.com>
2017-02-18 17:27:48 -0500

    Adding Angular Pages for Exercise

---
+++ public/exercise/exercise.js
@@ -0,0 +1,17 @@
+
+
+//Controller for exercise
+app.controller("exercise",function($scope,$location){
+  $scope.title = "";
+  $scope.description = "";
+  $scope.difficulty = "";
+  $scope.weight = 0;
+  $scope.records = [];
+
+  $scope.choose = function(choice){
+      $scope.difficulty = choice;
+  }
+  $scope.submit = function(){
+
+  }
+}
