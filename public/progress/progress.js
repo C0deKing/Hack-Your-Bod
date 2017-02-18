9b411cbfa4d045be0cd5066d5fa871beb3baf889
OtGabaldon <gabaldonot@gmail.com>
2017-02-18 11:29:31 -0500

    Added all but one chart

--- public/progress/progress.js
+++ public/progress/progress.js
@@ -4,9 +4,7 @@
         $scope.bodyFat="";
         $scope.waterWeight="";
         $scope.muslcePercent="";
-        $scope.macFat="";
-        $scope.macCarb="";
-        $scope.macProtein="";
+        $scope.macroNutrients="";//MUST BE IN ORDER OF CARB,PROTEIN,FAT
         $scope.micFiber="";
         $scope.micFluid="";
         $scope.micSugar="";
@@ -29,6 +27,73 @@
         });
     }
 
+    $scope.createBodyFatChart = function() {
+            var bodyFatChart = new Chart(ctx, {
+            type: 'line',
+            data: bodyFat,
+            options: {
+
+            yAxisID:"Body Fat %",
+            pointRadius:1,
+            pointHoverRadius:1.5,
+            pointHitRadius:1.5,
+            dataPoints: bodyFat
+
+        }
+        });
+
+    }
+
+    $scope.createWaterWeightChart = function () {
+            
+            var waterWeightChart = new Chart(ctx, {
+            type: 'line',
+            data: waterWeight,
+            options: {
+
+            yAxisID:"Water Weight (Pounds)",
+            pointRadius:1,
+            pointHoverRadius:1.5,
+            pointHitRadius:1.5,
+            dataPoints: waterWeight
+
+        }
+        });
+    }
+
+    $scope.createMusclePercentageChart = function () {
+
+            var waterWeightChart = new Chart(ctx, {
+            type: 'line',
+            data: waterWeight,
+            options: {
+
+            yAxisID:"Water Weight (Pounds)",
+            pointRadius:1,
+            pointHoverRadius:1.5,
+            pointHitRadius:1.5,
+            dataPoints: waterWeight
+
+        }
+    });
+    
+    $scope.createMacroNutrientChart = function (){
+        var myPieChart = new Chart(ctx,{
+        type: 'pie',
+        data: macroNutrients,
+        options: {
+
+            labels:["Carbs","Protein","Fats"],
+                animation:{
+                    animation.animateRotate : true
+                }
+
+        }
+    });
+
+    }
+
+    }
 
 
 
