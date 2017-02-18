04fc01450c51c3a624f55ca69af22c6d2c377429
Matt <matt@oaesoftware.com>
2017-02-18 02:32:35 -0500

    folder tweaks

--- public/controllers.js
+++ public/controllers.js
@@ -1,4 +1,4 @@
-var app = angular.module("app",  ["ngRoute"]);
+app = angular.module("app",  ["ngRoute"]);
 getDB();
 app.config(function($routeProvider) {
     $routeProvider
--- public/login/login.js
+++ public/login/login.js
@@ -1,9 +1,10 @@
+
 app.controller("login", function ($scope, $location) {
     $scope.email = "";
     $scope.password = "";
     $scope.errorCode = "";
     $scope.errorMessage = "";
-    $scope.login = function() {
+    $scope.logIn = function() {
     firebase.auth().signInWithEmailAndPassword($scope.email, $scope.password).catch(function(error) {
         $scope.errorCode = error.code;
         $scope.errorMessage = error.message;
