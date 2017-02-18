f3cc1b23b3726b693920d9d91f638700497b6f2f
Matt <matt@oaesoftware.com>
2017-02-18 05:15:21 -0500

    Adding Ot's routes

--- public/Register/register.js
+++ public/Register/register.js
@@ -8,40 +8,20 @@ app.controller("register",function($scope,$location) {
     $scope.BMR = "";
     $scope.UserName="";
     $scope.BMRcalc = function(){
-
         var BMR=0;
-
             if(sex=="male"){
-                
                 BMR=(66+(13.75($scope.InitBodyWeight))+(5*$scope.Height))-(6.76*$scope.age);
-
             }
             else{
-
                 BMR=(655+(9.56($scope.InitBodyWeight)+1.85($scope.Height)-4.68($scope.age)));
-
             }
-        
         $scope.BMR = BMR;
-
-        
-        
-
     }
-
     $scope.makeMale = function(){
-
         $scope.Sex="Male";
-
     }
-
     $scope.makeFemale = function(){
-
         $scope.Sex="Female";
-
     }
-
-
-
 
 });
