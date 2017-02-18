448f94ab5df443c4df34e695aee27cfec5887e7d
Matt <matt@oaesoftware.com>
2017-02-18 06:01:37 -0500

    adding ott's stuff

--- public/Register/register.js
+++ public/Register/register.js
@@ -1,26 +1,35 @@
 app.controller("register",function($scope,$location) {
-    $scope.Height=0;
+    $scope.Height="";
     $scope.Sex="";
-    $scope.age=0;
-    $scope.InitBodyWeight=0;
+    $scope.age="";
+    $scope.InitBodyWeight="";
     $scope.FirstName="";
-    $scope.LastName="Doe";
+    $scope.LastName="";
     $scope.BMR = "-----";
     $scope.BMRcalc = function(){
         var BMR=0;
-            if(sex=="male"){
-                BMR=(66+(13.75($scope.InitBodyWeight))+(5*$scope.Height))-(6.76*$scope.age);
+            if($scope.Sex=="Male"){
+                BMR=(66+(13.75 *($scope.InitBodyWeight))+(5*$scope.Height))-(6.76*$scope.age);
             }
             else{
-                BMR=(655+(9.56($scope.InitBodyWeight)+1.85($scope.Height)-4.68($scope.age)));
+                BMR=(655+(9.56 *($scope.InitBodyWeight)+1.85*($scope.Height)-4.68*($scope.age)));
             }
-        $scope.BMR = BMR;
+        $scope.BMR =  Math.round(BMR);
     }
     $scope.makeMale = function(){
         $scope.Sex="Male";
+
+        $("#male").removeClass("btn-default");
+        $("#female").removeClass("btn-primary");
+        $("#male").addClass("btn-primary");
+        $("#female").addClass("btn-default");
     }
     $scope.makeFemale = function(){
         $scope.Sex="Female";
+        $("#female").removeClass("btn-default");
+        $("#male").removeClass("btn-primary");
+        $("#female").addClass("btn-primary");
+        $("#male").addClass("btn-default");
     }
 
 });
