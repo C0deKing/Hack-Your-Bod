207346a39837a0a640283beeab9aa9c23b425f4c
OtGabaldon <gabaldonot@gmail.com>
2017-02-18 08:06:27 -0500

    Some form changes on the inputs

--- public/Register/register.js
+++ public/Register/register.js
@@ -5,16 +5,17 @@ app.controller("register",function($scope,$location) {
     $scope.InitBodyWeight="";
     $scope.FirstName="";
     $scope.LastName="";
-    $scope.BMR = "-----";
+    $scope.activity="";
+    $scope.BMR="-----"
     $scope.BMRcalc = function(){
-        var BMR=0;
+         var BMR=0;
             if($scope.Sex=="Male"){
-                BMR=(66+(13.75 *($scope.InitBodyWeight))+(5*$scope.Height))-(6.76*$scope.age);
+                BMR=((66+(13.75 *($scope.InitBodyWeight))+(5*$scope.Height))-(6.76*$scope.age))*$scope.activity;
             }
             else{
-                BMR=(655+(9.56 *($scope.InitBodyWeight)+1.85*($scope.Height)-4.68*($scope.age)));
+                 BMR=(655+(9.56 *($scope.InitBodyWeight)+1.85*($scope.Height)-4.68*($scope.age)))*$scope.activity;
             }
-        $scope.BMR =  Math.round(BMR);
+       $scope.BMR =  Math.round(BMR);
     }
     $scope.makeMale = function(){
         $scope.Sex="Male";
@@ -23,7 +24,8 @@ app.controller("register",function($scope,$location) {
         $("#female").removeClass("btn-primary");
         $("#male").addClass("btn-primary");
         $("#female").addClass("btn-default");
-    }
+
+        }
     $scope.makeFemale = function(){
         $scope.Sex="Female";
         $("#female").removeClass("btn-default");
